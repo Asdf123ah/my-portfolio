ca1324bbaa1de3a9753f14b1045b6d75dfe2fb8b
jonathan-guevarra <0320-0770@lspu.edu.ph>
2024-07-19 16:31:03 +0800

    feat: add google tag for google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { VortexDemoSecond } from "./contact";
 import { SkillDemo } from "./skill";
 import Link from "next/link";
 import UpArrowIcon from "./UpArrowIcon";
+import Head from "next/head";
 
 function BackButton() {
   return (
@@ -20,14 +21,34 @@ function BackButton() {
 
 export default function Home() {
   return (
-    <div className="bg-[#0a0a0a] overflow-x-hidden">
-      <NavbarDemo />
-      <BackgroundBeamsDemo />
-      <AboutDemo />
-      <VortexDemo />
-      <SkillDemo />
-      <VortexDemoSecond />
-      <BackButton />
-    </div>
+    <>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FK4K0N2H76"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FK4K0N2H76');
+        `,
+          }}
+        />
+      </Head>
+      <div className="bg-[#0a0a0a] overflow-x-hidden">
+        <NavbarDemo />
+        <BackgroundBeamsDemo />
+        <AboutDemo />
+        <VortexDemo />
+        <SkillDemo />
+        <VortexDemoSecond />
+        <BackButton />
+      </div>
+    </>
   );
 }
